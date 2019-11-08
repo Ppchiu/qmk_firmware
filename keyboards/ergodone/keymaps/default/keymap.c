03eff8e269c20938bae441a47b017154639135d6
Paul Chiu <paul.chiu@sophos.com>
2019-11-07 16:26:49 -0800

    fix LALT to KC_ALT

--- keyboards/ergodone/keymaps/default/keymap.c
+++ keyboards/ergodone/keymaps/default/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_EQL,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         KC_TAB,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   KC_DEL,
-        LT(SYMB,KC_GRV),KC_QUOT,      LALT,  KC_LEFT,KC_RGHT,
+        LT(SYMB,KC_GRV),KC_QUOT,      KC_LALT,  KC_LEFT,KC_RGHT,
                                               KC_INS,  KC_LGUI,
                                                               KC_HOME,
                                                KC_SPC,KC_BSPC,KC_END,
